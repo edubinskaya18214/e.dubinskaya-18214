55996e22d9d9237c7b45d987120d79f038eb3c1f
Екатерина <Bromkate@mail.ru>
2018-09-16 23:09:41 +0700

    123456

---
+++ Homework 2/Ох ты боже что то страшное.hs
@@ -0,0 +1,7 @@
+
+square a b c 
+          | a == 0 = (-c/b , -c/b)
+          |(d a b c <0) = error "No roots"
+		  |(d a b c ==0) = (-b/(2*a) , -b/ (2*a))
+		  |(d a b c >0) = (((- b - sqrt(d a b c))/(2*a)), ((-b -(sqrt (d a b c)))/(2*a)))
+	  where d a b c= b*b - 4*a*c
